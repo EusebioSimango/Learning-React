18d45147d1bc14848200f8866823f35abfd57d8c
Eusebio Simango <eusebiosimango14@gmail.com>
2022-04-15 00:24:50 +0200

    Learning React

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const Home = () => {
+  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'eusebio', id: 1 },
+    { title: 'Welcome party!', body: 'lotrm ipsum...', author: 'simango', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem iupsum...', author: 'eusebio', id: 3 }
+  ]);
+  
+  return (
+    <div className="Home"> 
+    {  }
+      { blogs.map( blog => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Home;
