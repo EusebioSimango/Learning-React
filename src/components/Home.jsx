3e31baf981d4773f1a8026aae1d58b9e944ee52f
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-18 17:48:19 +0200

    adding a home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 import styles from './Home.module.css'
+import useFetch from './useFetch'
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'eusebio', id: 1 },
-    { title: 'Welcome party!', body: 'lotrm ipsum...', author: 'simango', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem iupsum...', author: 'eusebio', id: 3 }
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id )
-    setBlogs(newBlogs)
-  }
-
+  const { data, isPending, error } = useFetch('http://localhost:8000/blogs')
+ 
   return (
     <div className="Home">
-      <BlogList blogs={blogs} styles={styles} title="All blogs!" handleDelete={handleDelete} />
-      {/* <BlogList blogs={blogs.filter(blog => blog.author === 'eusebio')} styles={styles} title="Eusebio's blogs!" /> */}
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div> }
+      { data && <BlogList blogs={data} styles={styles} title="All blogs!" />}
     </div>
   )
 }
